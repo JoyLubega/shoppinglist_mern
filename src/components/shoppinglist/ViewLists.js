e1e9edd7b282dfc30326e19912826c156ce841a6
JoyLubega <jollankent@gmail.com>
2018-12-22 21:55:47 +0300

    Add buy/unbuy

--- src/components/shoppinglist/ViewLists.js
+++ src/components/shoppinglist/ViewLists.js
@@ -3,20 +3,31 @@ import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
 
+class ViewShoppingLists extends React.Component{
+    constructor(props){
+      super(props);
+      this.state ={
+          data: 'Buy',
+          strike:false
+        };
+    }
 
-const ViewShoppingLists = (props) => {
-    const { 
-        shoppinglists,
-        deleteOneShoppingList,
-        updateOneShoppinglist,
-     } = props;
 
-    const renderShoppinglists = () =>  (
-        _.map(shoppinglists, shoppinglist => {
+    btnClick=()=>{
+    if (this.state.strike ===false) {
+        this.setState({data: 'unBuy', strike:true});
+    }else{
+        this.setState({data: 'Buy', strike:false})
+    }
+}
+       
+    renderShoppinglists = () =>  (
+        _.map(this.props.shoppinglists, shoppinglist => {
             const list_id = shoppinglist._id;
+            const strikeState = (this.state.strike === true) ? 'line-through': 'None' ;
             return (
-                <tr key={list_id}>
-                    <td>{shoppinglist.name}</td>
+                <tr key={list_id}  style={{ 'textDecoration': strikeState}}>
+                    <td  >{shoppinglist.name}</td>
                     <td>{shoppinglist.description}</td>
                     <td>
                         <Link 
@@ -25,12 +36,12 @@ const ViewShoppingLists = (props) => {
                             data-placement="top" 
                             title="View Items"  
                             to={`/${list_id}/${shoppinglist.name}/shoppingitems`}
-                        ><i class="far fa-eye"></i></Link>
+                        ><i className="far fa-eye"></i></Link>
                       
                         <button 
                             type="button" 
                             className="btn btn-outline-primary"
-                            onClick={()=>{updateOneShoppinglist(shoppinglist._id)}} 
+                            onClick={()=>{this.props.updateOneShoppinglist(shoppinglist._id)}} 
                             data-toggle="tooltip" 
                             data-placement="top" 
                             title="Update_List"
@@ -40,44 +51,49 @@ const ViewShoppingLists = (props) => {
                         <button 
                             type="button" 
                             className="btn btn-outline-danger"
-                            onClick={()=>{deleteOneShoppingList(shoppinglist._id)}} 
+                            onClick={()=>{this.props.deleteOneShoppingList(shoppinglist._id)}} 
                             data-toggle="tooltip" 
                             data-placement="top" 
                             title="Delete_item" 
-                            ><i class="fas fa-trash"></i></button>  
-                    </td>                  
+                            ><i class="fas fa-trash"></i></button>
+                    </td> 
+                    <td>
+                    <button type="button"  className="btn btn-outline-warning" onClick={this.btnClick}>{this.state.data}</button>    
+                    </td>                 
                 </tr>
                 
             );
         })
     );
-
-    return(
-        <div className="Shoppinglists container">
-            
-            <br /> <br/>
-            
-            <h2>Shopping Lists:</h2>
-            
-            <div className="col-sm-12">
-                <table className="table bordered">
-                    <thead>
-                        <tr>  
-                            <td>Name</td>
-                            <td>Description</td>
-                            <td>Actions</td>
-                        
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {renderShoppinglists()}
-                    </tbody>
-                </table>
+  
+    render()
+    {
+        
+      return <div className="Shoppinglists container">
                 
-            </div>
-        </div>
-    );
-
-}
-
-export default ViewShoppingLists;
+      <br /> <br/>
+      
+      <h2>Shopping Lists:</h2>
+      
+      <div className="col-sm-12">
+          <table className="table bordered">
+              <thead>
+                  <tr>  
+                      <td>Name</td>
+                      <td>Description</td>
+                      <td>Actions</td>
+                  
+                  </tr>
+              </thead>
+              <tbody>
+                  {this.renderShoppinglists()}
+              </tbody>
+          </table>
+          
+      </div>
+  </div>
+    }
+  
+  }
+  
+  export default ViewShoppingLists;
